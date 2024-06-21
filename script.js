4e0d0e84c20dddce5557090cd4b8d1ecda582e13
MitchKubina <mtkubina6@gmail.com>
2024-06-21 14:45:42 -0600

    added bar chart

--- script.js
+++ script.js
@@ -47,6 +47,37 @@ var cht = new Chart(ctx, {
     }
 });
 
+const bar = document.getElementById("barChart");
+var cht3 = new Chart(bar, {
+    type: 'bar',
+    data: {
+        labels: ['Travel', 'School', 'Leisure', 'Utilities', 'Rent', 'Groceries'],
+        datasets: [{
+            label: 'Total Spent',
+            data: [travel, school, leisure, utilities, rent, groceries],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)'
+            ],
+            borderWidth: 1
+        }],
+    }
+});
+
 revForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
@@ -99,7 +130,7 @@ function calcExpense() {
     for (let i in dat) {
         tota += dat[i];
     }
-    console.log(tota);
+    //console.log(tota);
     return tota;
 }
 
@@ -109,7 +140,10 @@ function updateChart() {
     dat2[1] = calcExpense();
     cht2.config.data.datasets[0].data = dat2;
 
+    cht3.config.data.datasets[0].data = dat;
+
     cht.update();
     cht2.update();
+    cht3.update();
 }
 
